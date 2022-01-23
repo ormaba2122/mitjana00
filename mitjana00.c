9dd520a2eff2576c66535415c8e5ec4fd6521ade
ormaba <ormaba2122@protonmail.com>
2022-01-23 20:07:51 +0100

    Conflicte 1 entre main i br0

--- mitjana00.c
+++ mitjana00.c
@@ -23,5 +23,6 @@ int main() {
 	}
 	mitjana=suma/numero_repeticions;
 	printf("La mitjana resultant és: %.2f\n",mitjana);	
-	return 0;
+	printf("La mitjana ha estat trobada amb èxit\n");	
+return 0;
 }
